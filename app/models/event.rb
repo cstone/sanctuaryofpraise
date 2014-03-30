9903807de2572235e59152da968fb0c5fb408515
Cavin <cstone2003@hotmail.com>
2014-03-30 08:47:47 -0500

    Add scopes to event model

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,3 +1,7 @@
 class Event < ActiveRecord::Base
   attr_accessible :address, :city, :description, :state, :time, :title, :url, :venue, :zip
+
+  scope :active, where("time >= ?", Time.now).order("time")
+  scope :upcoming, where("time >= ?", Time.now).order("time").limit(3)
+  scope :next_event, where("time >= ?", Time.now).order("time").limit(1)
 end
