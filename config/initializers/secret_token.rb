341570553103e495bc162ee6713350a691aedb5d
Cavin <cstone2003@hotmail.com>
2014-03-29 22:39:26 -0500

    Add Page Resource

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sanctuaryofpraise::Application.config.secret_token = '9335c12b8e09bb4a9cd1beda1ce09dda3ec4dda8dd505b3b9a0f1db41c1ec5db352228d5008706852c04953d4769adb0266f88924d9c4b51b7aed018a997b6f6'
