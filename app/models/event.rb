ed1edebf85ba1610ab0e23a77b405cb0ffbdb40f
Cavin <cstone2003@hotmail.com>
2014-04-02 20:16:47 -0500

    Add event validations

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,7 +1,9 @@
 class Event < ActiveRecord::Base
   attr_accessible :address, :city, :description, :state, :time, :title, :url, :venue, :zip
 
+  validates_presence_of :title, :description, :time
+
   scope :active, where("time >= ?", Time.now).order("time")
-  scope :upcoming, where("time >= ?", Time.now).order("time").limit(3)
+  scope :upcoming, where("time >= ?", Time.now).order("time").limit(5)
   scope :next_event, where("time >= ?", Time.now).order("time").limit(1)
 end
