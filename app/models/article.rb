8a44d56cc7c7e31388cffb78fc9d111a8fe015b1
Cavin <cstone2003@hotmail.com>
2014-04-02 20:15:02 -0500

    Add article validations

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,3 +1,6 @@
 class Article < ActiveRecord::Base
   attr_accessible :active, :allow_comments, :article_image, :body, :title
+
+  validates_presence_of :title, :body
+  validates_uniqueness_of :title
 end
