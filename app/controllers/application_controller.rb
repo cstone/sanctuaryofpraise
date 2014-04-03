e08b10d262639f63faaa3201e0e7682a49d0de45
Cavin <cstone2003@hotmail.com>
2014-04-02 22:21:52 -0500

    Add get_dynamic_content before filter to application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,9 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  before_filter :get_dynamic_content
+
+  def get_dynamic_content
+    @side_upcoming_events = Event.upcoming
+  end
 end
