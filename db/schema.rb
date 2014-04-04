e4bf2ceb60e6292463076aab22f1ffc1e39eb928
Cavin <cstone2003@hotmail.com>
2014-04-03 22:40:51 -0500

    Add DynamicContent model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20140403033430) do
+ActiveRecord::Schema.define(:version => 20140404033453) do
 
   create_table "active_admin_comments", :force => true do |t|
     t.string   "resource_id",   :null => false
@@ -54,6 +54,14 @@ ActiveRecord::Schema.define(:version => 20140403033430) do
     t.boolean  "active"
     t.datetime "created_at",     :null => false
     t.datetime "updated_at",     :null => false
+  end
+
+  create_table "dynamic_contents", :force => true do |t|
+    t.string   "title"
+    t.string   "key"
+    t.string   "value"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
   end
 
   create_table "events", :force => true do |t|
