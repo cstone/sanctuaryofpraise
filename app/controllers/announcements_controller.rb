dfa705a82bae8195b04bda6e18d50f39f24992a6
Cavin <cstone2003@hotmail.com>
2014-04-04 10:36:35 -0500

    Update AnnouncementsController with index and show actions

--- app/controllers/announcements_controller.rb
+++ app/controllers/announcements_controller.rb
@@ -1,2 +1,24 @@
-class AnnouncementsController < InheritedResources::Base
+class AnnouncementsController < ApplicationController
+  # GET /articles
+  # GET /articles.json
+  def index
+    @announcements = Announcement.active.page(params[:page]).per(10)
+
+    respond_to do |format|
+      format.html # index.html.erb
+      format.json { render json: @announcements }
+    end
+  end
+
+  # GET /articles/1
+  # GET /articles/1.json
+  def show
+    @announcement = Announcement.find(params[:id])
+
+    respond_to do |format|
+      format.html # show.html.erb
+      format.json { render json: @announcement }
+    end
+  end
 end
+
