8caf10afa0b2ec7e110391dea30ff5c4e37bbf88
Cavin <cstone2003@hotmail.com>
2014-04-03 23:42:18 -0500

    Add @site_name to application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,5 +5,6 @@ class ApplicationController < ActionController::Base
 
   def get_dynamic_content
     @side_upcoming_events = Event.upcoming
+    @site_name = DynamicContent.get_value(:home_content)
   end
 end
