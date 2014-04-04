08637b015e4719706f9cac3cc8632750494ffb09
Cavin <cstone2003@hotmail.com>
2014-04-04 11:14:05 -0500

    Add scopes to announcements model

--- app/models/announcement.rb
+++ app/models/announcement.rb
@@ -1,3 +1,15 @@
 class Announcement < ActiveRecord::Base
   attr_accessible :content, :expires, :title
+
+  after_save :delete_inactive
+
+  default_scope order('created_at DESC')
+
+  scope :active, where("expires <= ?", Time.now).order('created_at DESC')
+  scope :inactive, where("expires >= ?", Time.now).order('created_at DESC')
+
+
+  def delete_inactive
+    Announcement.inactive.destroy_all
+  end
 end
