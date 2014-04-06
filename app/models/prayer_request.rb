4088190e7b76eadbfe186fc9f846fc270e31965e
Cavin <cstone2003@hotmail.com>
2014-04-05 19:44:51 -0500

    Add active scope

--- app/models/prayer_request.rb
+++ app/models/prayer_request.rb
@@ -1,6 +1,8 @@
 class PrayerRequest < ActiveRecord::Base
   attr_accessible :approved, :content, :title, :allow_comments
 
+  scope :active, where(approved: true)
+
   def to_param
     "#{id}-#{title.parameterize}"
   end
