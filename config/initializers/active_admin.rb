86882010c6d625ca77404f571291a6696026d6b3
Cavin <cstone2003@hotmail.com>
2014-04-06 14:10:58 -0500

    Change site title to "S.O.P.F."

--- config/initializers/active_admin.rb
+++ config/initializers/active_admin.rb
@@ -5,7 +5,7 @@ ActiveAdmin.setup do |config|
   # Set the title that is displayed on the main layout
   # for each of the active admin pages.
   #
-  config.site_title = "Sanctuary of Praise Fellowship"
+  config.site_title = "S.O.P.F."
 
   # Set the link url for the title. For example, to take
   # users to your main site. Defaults to no link.
