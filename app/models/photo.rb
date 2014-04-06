7713ea237c1b2281e3de0f8fcd99a1b9b7043dd2
Cavin <cstone2003@hotmail.com>
2014-04-05 22:49:12 -0500

    Add photo_image validation

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -2,4 +2,6 @@ class Photo < ActiveRecord::Base
   attr_accessible :allow_comments, :description, :photo_image, :title
 
   mount_uploader :photo_image, PhotoImageUploader
+
+  validates_presence_of :photo_image
 end
