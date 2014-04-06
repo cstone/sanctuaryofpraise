70fdef192e77564392aaa9323616bd8f10bec25b
Cavin <cstone2003@hotmail.com>
2014-04-06 14:10:39 -0500

    Add instance variables for media index page content

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,5 +8,19 @@ class ApplicationController < ActionController::Base
     @side_announcements = Announcement.featured
     @site_name = DynamicContent.get_value(:site_name)
     @home_content = DynamicContent.get_value(:home_content).html_safe
+    @events_home = DynamicContent.get_value(:events_home).html_safe
+    @blog_home = DynamicContent.get_value(:blog_home).html_safe
+    @prayer_home = DynamicContent.get_value(:prayer_home).html_safe
+    @announcements_home = DynamicContent.get_value(:announcements_home).html_safe
+    @photos_home = DynamicContent.get_value(:photos_home).html_safe
+    @videos_home = DynamicContent.get_value(:videos_home).html_safe
+    @visitors = DynamicContent.get_value(:visitors).html_safe
+    @visitors_title = DynamicContent.get_title(:visitors).html_safe
+    @about_sopf = DynamicContent.get_value(:about_sopf).html_safe
+    @about_sopf_title = DynamicContent.get_title(:about_sopf).html_safe
+    @location = DynamicContent.get_value(:location).html_safe
+    @location_title = DynamicContent.get_title(:location).html_safe
+    @service_time = DynamicContent.get_value(:service_time).html_safe
+    @service_time_title = DynamicContent.get_title(:service_time).html_safe
   end
 end
