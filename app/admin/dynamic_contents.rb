7bf8fa6f7a62fa4a137ec4d5939f5df0aca954d5
Cavin <cstone2003@hotmail.com>
2014-04-05 21:20:01 -0500

    Update dynamic contents in ActiveAdmin

--- app/admin/dynamic_contents.rb
+++ app/admin/dynamic_contents.rb
@@ -1,3 +1,46 @@
 ActiveAdmin.register DynamicContent do
-  
+
+  form do |f|
+    f.inputs do
+      f.input :title
+      f.input :key
+      f.input :value, as: :html_editor
+    end
+    f.actions
+  end
+
+
+  index do
+    selectable_column
+    column :title
+    column :key
+    column :value do |value|
+      value.value.truncate(75).html_safe
+    end
+    default_actions
+  end
+
+  filter :title
+  filter :key
+  filter :value
+
+
+  show do |content|
+    attributes_table do
+      row :id
+      row :title
+      row :key
+      row :value do
+        content.value.html_safe
+      end
+      row :created_at do
+        content.created_at.to_formatted_s(:default)
+      end
+      row :updated_at do
+        content.updated_at.to_formatted_s(:default)
+      end
+    end
+    active_admin_comments
+  end
+
 end
