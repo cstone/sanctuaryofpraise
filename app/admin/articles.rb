971a7db361b5e3e538f8b61122a86b76ac4a5783
Cavin <cstone2003@hotmail.com>
2014-04-05 21:20:37 -0500

    Update Articles in ActiveAdmin

--- app/admin/articles.rb
+++ app/admin/articles.rb
@@ -16,8 +16,31 @@ ActiveAdmin.register Article do
     selectable_column
     column :article_image
     column :title
-    column :body
+    column :body do |body|
+      body.body.truncate(75).html_safe
+    end
     default_actions
+  end
+
+
+  show do |article|
+    attributes_table do
+      row :id
+      row :title
+      row :body do
+        article.body.html_safe
+      end
+      row :article_image
+      row :allow_comments
+      row :active
+      row :created_at do
+        article.created_at.to_formatted_s(:default)
+      end
+      row :updated_at do
+        article.updated_at.to_formatted_s(:default)
+      end
+    end
+    active_admin_comments
   end
   
 end
