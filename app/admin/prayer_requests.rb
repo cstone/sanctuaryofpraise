4b52e5cc27db19e2d0e77dd4cd9099d669914a2a
Cavin <cstone2003@hotmail.com>
2014-04-05 21:43:30 -0500

    Update Prayer Requests in ActiveAdmin

--- app/admin/prayer_requests.rb
+++ app/admin/prayer_requests.rb
@@ -1,3 +1,47 @@
 ActiveAdmin.register PrayerRequest do
-  
+
+  form do |f|
+    f.inputs do
+      f.input :title
+      f.input :content, as: :html_editor
+      f.input :allow_comments
+      f.input :approved
+    end
+    f.actions
+  end
+
+
+  index do
+    selectable_column
+    column :title
+    column :content do |content|
+      content.content.truncate(75).html_safe
+    end
+    column :allow_comments
+    column :approved
+    default_actions
+  end
+
+
+
+  show do |prayer|
+    attributes_table do
+      row :id
+      row :title
+      row :content do
+        prayer.content.html_safe
+      end
+      row :allow_comments
+      row :approved
+      row :created_at do
+        prayer.created_at.to_formatted_s(:default)
+      end
+      row :updated_at do
+        prayer.updated_at.to_formatted_s(:default)
+      end
+    end
+    active_admin_comments
+  end
+
+
 end
