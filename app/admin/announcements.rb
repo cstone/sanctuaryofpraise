676b477888e4557d2b3a37d1a4035c4b3ad9fcb5
Cavin <cstone2003@hotmail.com>
2014-04-05 20:39:41 -0500

    Update announcement pages in ActiveAdmin

--- app/admin/announcements.rb
+++ app/admin/announcements.rb
@@ -1,3 +1,37 @@
 ActiveAdmin.register Announcement do
-  
+
+  index do
+    selectable_column
+    column :title do |title|
+      title.title.truncate(50)
+    end
+    column :content do |content|
+      content.content.truncate(50)
+    end
+    column :expires do |expire|
+      expire.expires.to_formatted_s(:default)
+    end
+    column :allow_comments
+    default_actions
+  end
+
+  filter :title
+  filter :content
+  filter :expires
+  filter :allow_comments
+
+
+
+
+
+  form do |f|
+    f.inputs do
+      f.input :title
+      f.input :content, as: :html_editor
+      f.input :expires, as: :just_datetime_picker
+      f.input :allow_comments
+    end
+    f.actions
+  end
+
 end
