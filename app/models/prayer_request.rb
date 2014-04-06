baf79d7519dbd40d2fa96ff15f3361250d43bf65
Cavin <cstone2003@hotmail.com>
2014-04-06 14:56:42 -0500

    Add validation for presence of title and content on prayer request model

--- app/models/prayer_request.rb
+++ app/models/prayer_request.rb
@@ -1,6 +1,8 @@
 class PrayerRequest < ActiveRecord::Base
   attr_accessible :approved, :content, :title, :allow_comments
 
+  validates_presence_of :title, :content
+
   scope :active, where(approved: true)
 
   def to_param
