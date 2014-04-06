7ea4245794129cb8c439d017152ecae77f38b4eb
Cavin <cstone2003@hotmail.com>
2014-04-06 14:58:13 -0500

    Add validation for presence of title, description and source on video model

--- app/models/video.rb
+++ app/models/video.rb
@@ -1,6 +1,9 @@
 class Video < ActiveRecord::Base
   attr_accessible :allow_comments, :description, :source, :title
 
+  validates_presence_of :title, :description, :source
+  validates_uniqueness_of :source
+
   def to_param
     "#{id}-#{title.parameterize}"
   end
