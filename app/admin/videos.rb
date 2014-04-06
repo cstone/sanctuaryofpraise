1dfc7385e042289c02c7cbd3b86e88f8d0ab52b4
Cavin <cstone2003@hotmail.com>
2014-04-05 21:55:52 -0500

    Update Videos in ActiveAdmin

--- app/admin/videos.rb
+++ app/admin/videos.rb
@@ -1,4 +1,48 @@
 ActiveAdmin.register Video do
 
+
+  index do
+    selectable_column
+    column :title
+    column :description do |description|
+      description.description.truncate(75)
+    end
+    column :source
+    column :allow_comments
+    default_actions
+  end
+
+
+  form do |f|
+    f.inputs do
+      f.input :title
+      f.input :description, as: :html_editor
+      f.input :source
+      f.input :allow_comments
+    end
+    f.actions
+  end
+
+
+
+  show do |video|
+    attributes_table do
+      row :id
+      row :title
+      row :description do
+        video.description.html_safe
+      end
+      row :source
+      row :allow_comments
+      row :created_at do
+        video.created_at.to_formatted_s(:default)
+      end
+      row :updated_at do
+        video.updated_at.to_formatted_s(:default)
+      end
+    end
+    active_admin_comments
+  end
+
   
 end
