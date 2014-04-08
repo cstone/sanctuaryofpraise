3e673136b18d7fd05b9cd512209fcb16fb58f45b
Cavin <cstone2003@hotmail.com>
2014-04-07 22:13:47 -0500

    Add ArticleImageUploader

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,6 +1,8 @@
 class Article < ActiveRecord::Base
   attr_accessible :active, :allow_comments, :article_image, :body, :title
 
+  mount_uploader :article_image, ArticleImageUploader
+
   default_scope order("created_at DESC")
 
   scope :active,  where(active: true)
