b8419bc40f7e25ce38dadd97036ab7a8ac015596
Cavin <cstone2003@hotmail.com>
2014-04-07 22:40:53 -0500

    Add :article_image to index, show and form in ActiveAdmin

--- app/admin/articles.rb
+++ app/admin/articles.rb
@@ -4,7 +4,11 @@ ActiveAdmin.register Article do
     f.inputs do
       f.input :title
       f.input :body, as: :html_editor
-      f.input :article_image
+      f.input :article_image, :as => :file, :hint => f.object.article_image.present? \
+    ? f.template.image_tag(f.object.article_image.url(:thumb))
+      : f.template.content_tag(:span, "no photo yet")
+      f.input :article_image_cache, :as => :hidden
+      f.input :remove_article_image, as: :boolean, label: 'Check here to remove the current image.'
       f.input :allow_comments
       f.input :active
     end
@@ -14,7 +18,11 @@ ActiveAdmin.register Article do
 
   index do
     selectable_column
-    column :article_image
+    column :article_image do |article|
+      if article.article_image.url
+        image_tag article.article_image.url(:thumb)
+      end
+    end
     column :title
     column :body do |body|
       body.body.truncate(75).html_safe
@@ -30,7 +38,11 @@ ActiveAdmin.register Article do
       row :body do
         article.body.html_safe
       end
-      row :article_image
+      row :article_image do
+        if article.article_image.url
+          image_tag article.article_image.url
+        end
+      end
       row :allow_comments
       row :active
       row :created_at do
